1e056925def66f762a73c0a94d764f027281042d
tngon462 <tngon462@gmail.com>
2026-03-02 08:30:49 +0900

    Update app-config.js

--- IN/web/assets/js/app-config.js
+++ IN/web/assets/js/app-config.js
@@ -1,26 +1,19 @@
-// Global config + localStorage keys
-
+// IN/web/assets/js/app-config.js
 export const LS_KEYS = {
   SETTINGS: "tngon_label_settings_v1",
   TEMPLATE_JSON: "tngon_label_template_json_v1",
-  LAST_FORM: "tngon_label_print_form_v1",
 };
 
 export const DEFAULTS = {
   settings: {
-    hubUrl: "http://192.168.58.113:8787",
-    token: "YOUR_TOKEN",
+    hub_url: "http://192.168.58.113:8787",
+    hub_token: "YOUR_TOKEN",
     printer: "",
     copies: 1,
-    label: { width_mm: 50, height_mm: 30, dpi: 203 },
-    requestTimeoutMs: 12000,
-  },
-  printForm: {
-    baseCode: "123456",
-    name: "Thịt bò",
-    pricePerKg: 10000,
-    minGram: 500,
-    maxGram: 2000,
-    stepGram: 50,
+    label: {
+      width_mm: 50,
+      height_mm: 30,
+      dpi: 203,
+    }
   }
 };
